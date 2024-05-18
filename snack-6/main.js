fbeeefa0175bbf48e663298debc9b31d91f0182d
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-18 11:38:20 +0200

    add: snack-6

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,1 +1,21 @@
 'use strict';
+//dichiaro la variabile num e faccio generare un numero casuale da 1 a 10 al computer
+//il numero lo converto in un intero
+let num = parseInt(Math.random()*10);
+//stampo il numero generato
+console.log(num);
+
+
+//chiedo di digitare un numero
+const userNum = prompt("digita un numero");
+//converto la stringa in numero effettivo
+Number.parseInt(userNum);
+
+
+if(isNaN(userNum)){
+    window.alert("scrivi un numero per piacere");
+}else if(userNum == num){
+    window.alert("Hai vinto!");
+}else{
+    window.alert("Hai perso :(");
+}
