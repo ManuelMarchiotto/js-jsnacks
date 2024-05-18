0ead9d7b3b9fbe924b78aa2e78eecbb2dae58263
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-18 11:21:27 +0200

    add: snack-7

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,1 +1,9 @@
 'use strict';
+//ciclo for per arrivare fino a mille
+for( let i = 0; i <=1000; i++){
+    //dichiaro la variabile i e eseguo la tabellina del 2
+    let num = i*2;
+    console.log(num); 
+}
+
+
