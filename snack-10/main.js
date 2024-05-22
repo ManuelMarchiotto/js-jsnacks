14c14eb0f8cb139b6813642287d284491efd2540
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-22 15:51:48 +0200

    add: snack10

--- snack-10/main.js
+++ snack-10/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let num1 = null;
+
+for(let i = 0; i < 100; i++){
+
+    let num2 = prompt('Inserisci un numero');
+
+    if(num1 === num2){
+        console.log('hai vinto');
+        break
+    }else{
+        num1 = num2;
+    }
+}
+
+
