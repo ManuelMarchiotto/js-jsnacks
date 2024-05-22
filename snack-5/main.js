57ada9bdf1e70f2c38d3b466f496a3860f1b167b
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-22 15:04:16 +0200

    add: sanck5

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//creazione array vuoto
+const num = [];
+//stampo per verificare che sia vuoto
+console.log(num);
+
+//chiedo all'utente i 6 numeri
+for(let i = 1; i < 6; i++){
+    //chiedo all'utente i numeri
+    let inputNum = Number(prompt(`inserisci il ${i} numero`));
+    //verifico sel il numero inserito è dispari
+    if(!isNaN(inputNum)){
+        if(inputNum % 2 !== 0){
+            num.push(inputNum);
+        }
+    } 
+    }
+
+
+//stampo array completo
+console.log(num);
+
