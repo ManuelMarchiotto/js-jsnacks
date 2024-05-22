34ff925f76a9f6ed23767de461b2314973f976ed
Manuel Marchiotto <manuelmarchiotto007@gmail.com>
2024-05-22 15:44:57 +0200

    fix: snack7

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,9 +1,19 @@
 'use strict';
 //ciclo for per arrivare fino a mille
-for( let i = 0; i <=1000; i++){
-    //dichiaro la variabile i e eseguo la tabellina del 2
-    let num = i*2;
-    console.log(num); 
-}
+//for( let i = 0; i <=1000; i++){
+//    dichiaro la variabile i e eseguo la tabellina del 2
+//    let num = i*2;
+//    console.log(num); 
+//}
 
+const input = Number(prompt('inserisci il valore massimo che vuoi'));
+
+
+if (!isNaN(input)){
+    for (let i = 2; i <= input; i+=2 ){
+        console.log(i);
+    }
+}else {
+        console.log('non hai inseritu un valore valido');
+}
 
